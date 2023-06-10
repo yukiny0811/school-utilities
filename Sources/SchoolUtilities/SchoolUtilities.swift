63d8deb5adfb9d94bc6fc1f6b75017f772b186b5
yukiny0811 <yukiny0811@gmail.com>
2023-06-10 14:34:51 +0900

    UIImage+ Data+

--- Sources/SchoolUtilities/SchoolUtilities.swift
+++ Sources/SchoolUtilities/SchoolUtilities.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import UIKit
+import Foundation
+
+public extension UIImage {
+    func toData() -> Data? {
+        self.jpegData(compressionQuality: 1.0)
+    }
+}
+
+public extension Data {
+    func jpeg() -> UIImage? {
+        UIImage(data: self)
+    }
+}
