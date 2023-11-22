7d7b5f57ae7d0408bfb115b7332b4b25d82c0840
yukiny0811 <yukiny0811@gmail.com>
2023-11-22 19:55:20 +0900

    fix

--- Sources/SchoolUtilities/SchoolUtilities.swift
+++ Sources/SchoolUtilities/SchoolUtilities.swift
@@ -6,22 +6,7 @@
 //import UIKit
 import Foundation
 
-//public extension UIImage {
-//    func toData() -> Data? {
-//        self.jpegData(compressionQuality: 1.0)
-//    }
-//}
-//
-//public extension Data {
-//    func jpeg() -> UIImage? {
-//        UIImage(data: self)
-//    }
-//}
-
 public extension CGPoint {
-    init(x: Float, y: Float) {
-        self = CGPoint(x: CGFloat(x), y: CGFloat(y))
-    }
     func distance(from other: CGPoint) -> CGFloat {
         let first = (other.x - self.x) * (other.x - self.x)
         let second = (other.y - self.y) * (other.y - self.y)
@@ -31,18 +16,6 @@ public extension CGPoint {
 extension CGPoint: Identifiable {
     public var id: String {
         String(Float(self.x)) + String(Float(self.y))
-    }
-}
-
-//public extension UIColor {
-//    convenience init(red: Float, green: Float, blue: Float, alpha: Float) {
-//        self.init(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(alpha))
-//    }
-//}
-
-public extension CGSize {
-    init(width: Float, height: Float) {
-        self = CGSize(width: CGFloat(width), height: CGFloat(height))
     }
 }
 
